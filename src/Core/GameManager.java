c2c44f85b0ace583b4c3afecde6d32ac3b4c3f41
Oktay Özel <mail.oktayozel@gmail.com>
2025-12-05 17:48:42 -0500

    modified the gamemanager to be a abstract class and moved the monster and heroes logic to games/monstersandheroes/MaHGameManager.java

--- src/Core/GameManager.java
+++ src/Core/GameManager.java
@@ -1,14 +1,9 @@
 package src.Core;
 
-import src.IO.Output;
-import src.IO.Input;
-
 import java.util.Random;
 
-import src.Battle.Battle;
-import src.Default.DefaultReader;
 
-public class GameManager {
+public abstract class GameManager {
 
     private Random rand = new Random();
 
@@ -24,86 +19,10 @@ public class GameManager {
     }
 
 
-    // setup user party board and starting position
-    private void setupGame() {
-        Output.gameInitializationMessage();
-        Output.someSpace();
 
-        String name = Input.getUsername();
-        this.user = new User(name);
-
-        int partySize = Input.getPartySize();
-        this.user.getParty().initializeParty(partySize);
-
-
-        // make board until its playable
-        this.board = new Board(DefaultReader.getDefaultSettings("board_size"));
-        while(!board.isBoardPlayable()){
-            this.board = new Board(DefaultReader.getDefaultSettings("board_size"));
-        }
-
-
-        // find first accessible tile for party
-        int startRow = 0; 
-        int startCol = 0;
-        boolean found = false;
-        for (int r = 0; r < board.getSize() && !found; r++) {
-            for (int c = 0; c < board.getSize(); c++) {
-                if (board.getTile(r, c).isAccessible()) {
-                    startRow = r;
-                    startCol = c;
-                    found = true;
-                    break;   
-                }
-            }
-        }
-
-        this.partyPiece = new Piece(startRow, startCol);
-    }
-
-
-    // main game loop
-    public void start() {
-
-        Output.displaySecondWelcomeMessage(user);
-        Input.setStatistics(statistics);
-        
-        boolean running = true;
-        while (running) {
-            if (!user.isInMarket() && !user.isInBattle()) {
-                board.printBoard(partyPiece.getRow(), partyPiece.getCol());
-                Output.printMenu();
-                running = Input.getInput(this);
-            }
-        }
-        Output.clearScreen();
-        Output.displayStatistics(statistics);
-        System.out.println(Output.BRIGHT_YELLOW + "Press ENTER to continue..." + Output.RESET);
-        Input.waitForEnter();
-        Output.print("Thanks for playing!");
-    }
-
-
-    // random battle on common tile
-    public void handleTileEvent() {
-        Tile tile = board.getTile(partyPiece.getRow(), partyPiece.getCol());
-        if (tile == null) {
-            return;
-        }
-
-        if (tile.isCommon()) {
-            int encounterChance = rand.nextInt(100);
-            if (encounterChance < DefaultReader.getDefaultSettings("battle_probability_percent")) {
-                Output.narrative("OH NO! Monsters here! Battle starts now!", Output.RED);
-                user.setInBattle(true);
-                Battle battle = new Battle(this);
-                battle.start();
-            }
-            else {
-                Output.printRandomNoBattleMessage(encounterChance);
-            }
-        }
-    }
+    abstract public void start();
+    abstract public void handleTileEvent();
+    abstract public void setupGame();
 
     public Board getBoard() {
         return board;
