c09c096086997546a3f9e92ed7d7bfcca44909bf
Oktay Özel <mail.oktayozel@gmail.com>
2025-12-10 17:37:28 -0500

    hero generation

--- src/Games/LegendsOfValor/LoVGameManager.java
+++ src/Games/LegendsOfValor/LoVGameManager.java
@@ -1,13 +1,25 @@
 package src.Games.LegendsOfValor;
 
-import src.Core.GameManager;
-import src.Utils.IO.Input;
-import src.Utils.IO.Output;
-import src.Utils.Statistics.Statistics;
+import src.Hero.Hero;
+import src.Hero.Paladin;
+import src.Hero.Sorcerer;
+import src.Hero.Warrior;
+import src.Inventory.Inventory;
 import src.Utils.Default.DefaultReader;
+import java.util.ArrayList;
+import java.util.List;
+import src.Core.GameManager;
+import src.Utils.Statistics.Statistics;
+import src.Utils.IO.Input;;
+import src.Utils.IO.Output;
+
 import src.Core.User;
 
 public class LoVGameManager extends GameManager {
+    private List<Hero> heroes = new ArrayList<>();
+    private List<String> heroLanes = new ArrayList<>();
+    // Ask player for hero type and lane, create heroes
+
     
     private User user;
 
@@ -22,12 +34,12 @@ public class LoVGameManager extends GameManager {
 
 
         this.user = new User(Input.getUsername());
-
-
+        initializeHeroes();
         this.board = new LoVBoard();
         while(!board.isBoardPlayable()){
             this.board = new LoVBoard();
-        }   
+        }
+        // TODO: Place heroes on board according to heroLanes
 
     }
 
@@ -50,6 +62,49 @@ public class LoVGameManager extends GameManager {
         Output.print("Thanks for playing!");
     }
 
+
+    public void initializeHeroes() {
+        String[] laneNames = {"left", "mid", "right"};
+        for (int i = 1; i <= 3; i++) {
+            Output.print("Select type for Hero " + i + " (1: Warrior, 2: Sorcerer, 3: Paladin): ");
+            int heroType = Input.readInt(1, 3);
+            String classPlural = heroType == 1 ? "Warriors" : heroType == 2 ? "Sorcerers" : "Paladins";
+            DefaultReader.HeroTemplate chosen = DefaultReader.getRandomHero(classPlural);
+
+            // Null check and fallback
+            while (chosen == null) {
+                Output.print("No hero found for type " + classPlural + ". Please select again.");
+                heroType = Input.readInt(1, 3);
+                classPlural = heroType == 1 ? "Warriors" : heroType == 2 ? "Sorcerers" : "Paladins";
+                chosen = DefaultReader.getRandomHero(classPlural);
+            }
+
+            Inventory inv = new Inventory();
+            Hero hero;
+            String romanNumber = (i==1?"I": i==2?"II":"III");
+
+            if ("Warriors".equals(classPlural)) {
+                Warrior warrior = new Warrior(chosen.name + " " + romanNumber, chosen.level, chosen.HP, chosen.MP,
+                    chosen.strength, chosen.dexterity, chosen.agility, chosen.gold, inv);
+                warrior.equipDefaultWeapon();
+                hero = warrior;
+            } else if ("Sorcerers".equals(classPlural)) {
+                Sorcerer sorcerer = new Sorcerer(chosen.name + " " + romanNumber, chosen.level, chosen.HP, chosen.MP,
+                    chosen.strength, chosen.dexterity, chosen.agility, chosen.gold, inv);
+                sorcerer.equipDefaultWeapon();
+                hero = sorcerer;
+            } else {
+                Paladin paladin = new Paladin(chosen.name + " " + romanNumber, chosen.level, chosen.HP, chosen.MP,
+                    chosen.strength, chosen.dexterity, chosen.agility, chosen.gold, inv);
+                paladin.equipDefaultWeapon();
+                hero = paladin;
+            }
+            heroes.add(hero);
+            Output.print("Select lane for Hero " + i + " (1: left, 2: mid, 3: right): ");
+            int laneChoice = Input.readInt(1, 3);
+            heroLanes.add(laneNames[laneChoice-1]);
+        }
+    }
 
     @Override
     public void handleTileEvent() {
